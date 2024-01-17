2ff86f8442edd8b8c31b45e0a4dfebf4a7f3e7ac
aklix <alexandre.picas@estudiantat.upc.edu>
2024-01-17 19:56:11 +0100

    Update Image import in CharacterDisplay.tsx

--- components/CharacterDisplay.tsx
+++ components/CharacterDisplay.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 // Assuming you have a Character interface defined elsewhere
 import { Character } from '../components/utils/Character';
