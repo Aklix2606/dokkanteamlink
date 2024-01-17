4e5d950503557426bfde61eaea0e344005e8944e
aklix <alexandre.picas@estudiantat.upc.edu>
2024-01-17 19:57:33 +0100

    Fix import statement in Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import useRouter from 'next/router';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
